0b4958b953df8ffa382e899f963ca4ab86d99fe9
ryanromaris <ryanromaris@naver.com>
2021-10-21 10:16:39 +0900

    Dev:Footer , background color

--- client_winefind/components/Footer.js
+++ client_winefind/components/Footer.js
@@ -1,5 +1,27 @@
+import styles from "../styles/Home.module.css";
 const Footer = () => {
-  return <>Copyright ⓒ 2021. Apoint. All rights reserved. </>;
+  return (
+    <div className={styles.footer_container}>
+      <div className={styles.contactus}>
+        <h3>Contact Us</h3>
+
+        <div>
+          <a href="https://github.com/codestates/WIneFInd">
+            <img src="/images/github.png" height="50px" width="50px"></img>
+          </a>
+          &nbsp;&nbsp;&nbsp;
+          <a href="https://localhost:3000/inquiry">
+            <img src="/images/inquiry.png" height="50px" width="50px"></img>
+          </a>
+        </div>
+      </div>
+      <br />
+      <br />
+      <div className={styles.footer_rights}>
+        Copyright ⓒ 2021. Apoint. All rights reserved.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
