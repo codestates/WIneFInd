dc40fb96a3e474371fde09045db7226576bc6883
ryanromaris <ryanromaris@naver.com>
2021-10-25 15:45:21 +0900

    Dev: mall working

--- client_winefind/components/Article.js
+++ client_winefind/components/Article.js
@@ -1,21 +1,30 @@
+import { useEffect } from "react";
 import styles from "../styles/Article.module.css";
-const Article = ({ article }) => {
+const Article = ({ articles }) => {
   //users_id ==판매자 아이디
   //id == 게시글 아이디
   //wine id == 와인 아이디
-  //   const { id, users_id, wine_id, title, comment, image } = article;
+  // const { id, users_id, wine_id, title, comment, image } = article;
+
   return (
-    <div className={styles.article_container}>
-      <div className={styles.article_box}>
-        <img className={styles.wine_image} src="images/vina_ardanza.png" />
-        <div className={styles.wine_info}>
-          <div className="text">와인명 : vina_ardanzaaaaaaaaaaaaaaaaaaaaaa</div>
-          <div className="text">종류 : 레드와인</div>
-          <div className="text">국가 : 칠레</div>
-          <div className="text">가격 : 130,000 원</div>
+    <>
+      {articles.map((article, index) => (
+        <div className={styles.article_container} key={index}>
+          <div className={styles.article_box}>
+            <img className={styles.wine_image} src={article.image} />
+            <div className={styles.wine_info}>
+              <div className="text">
+                와인명 : vina_ardanzaaaaaaaaaaaaaaaaaaaaaa
+              </div>
+              <div className="text">종류 : 레드와인</div>
+              <div className="text">국가 : 칠레</div>
+              <div className="text">가격 : 130,000 원</div>
+              <div className="text">작성자 : {article.user.email}</div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
 
