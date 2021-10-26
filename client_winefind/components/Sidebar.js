167a4a24c1a2cbb26e0127368a33ae75adc2e4d0
sehan95 <sehan95@gmail.com>
2021-10-26 15:04:34 +0900

    feat: mypage done

--- client_winefind/components/Sidebar.js
+++ client_winefind/components/Sidebar.js
@@ -1,73 +1,67 @@
-import { useRouter } from "next/dist/client/router";
-import { Icon, Menu, Sticky } from "semantic-ui-react";
-import styles from "../styles/Home.module.css";
+import { useRouter } from 'next/dist/client/router';
+import { Icon, Menu, Sticky } from 'semantic-ui-react';
+import styles from '../styles/User.module.css';
 
-export default function Sidebar() {
+export default function Sidebar({ handleIconClick, Myprofile }) {
   const router = useRouter();
   let activeItem;
 
-  if (router.pathname === "/user") {
-    activeItem = "user";
-  } else if (router.pathname === "/shoppinglist") {
-    activeItem = "shoppinglist";
-  } else if (router.pathname === "/myprofile") {
-    activeItem = "myprofile";
-  } else if (router.pathname === "/resign") {
-    activeItem = "resign";
+  if (router.pathname === '/user') {
+    activeItem = 'user';
+  } else if (router.pathname === '/shoppinglist') {
+    activeItem = 'shoppinglist';
+  } else if (router.pathname === '/myprofile') {
+    activeItem = 'myprofile';
+  } else if (router.pathname === '/resign') {
+    activeItem = 'resign';
   }
 
   function goLink(e, data) {
-    if (data.name === "user") {
-      router.push("/user");
-    } else if (data.name === "shoppinglist") {
-      router.push("/shoppinglist");
-    } else if (data.name === "myprofile") {
-      router.push("/myprofile");
-    } else if (data.name === "resign") {
-      router.push("/resign");
+    if (data.name === 'user') {
+      router.push('/user');
+    } else if (data.name === 'shoppinglist') {
+      router.push('/shoppinglist');
+    } else if (data.name === 'myprofile') {
+      router.push('/myprofile');
+    } else if (data.name === 'resign') {
+      router.push('/resign');
     }
   }
 
   return (
     <Sticky
       style={{
-        position: "sticky",
-        top: "100px",
-        marginRight: "80rem",
+        position: 'sticky',
+        top: '10px',
+        marginTop: '-40rem',
       }}
     >
-      <Menu
-        icon="labeled"
-        vertical="true"
-        style={{
-          margin: "20px 10rem",
-          backgroundColor: "rgb(252, 241, 255)",
-        }}
-      >
-        <Menu.Item name="user" active={activeItem === "user"} onClick={goLink}>
-          <Icon name="list layout" />내 와인
+      <Menu icon='labeled' vertical='true' className={styles.sidebar}>
+        <Menu.Item name='user' active={activeItem === 'user'} onClick={goLink}>
+          <Icon name='list layout' />내 셀러
         </Menu.Item>
         <Menu.Item
-          name="shoppinglist"
-          active={activeItem === "shoppinglist"}
+          name='shoppinglist'
+          active={activeItem === 'shoppinglist'}
           onClick={goLink}
         >
-          <Icon name="shopping cart" /> 장바구니
+          <Icon name='shopping cart' /> 장바구니
         </Menu.Item>
 
         <Menu.Item
-          name="myprofile"
-          active={activeItem === "myprofile"}
+          name='myprofile'
+          activeItem={activeItem === 'myprofile'}
           onClick={goLink}
         >
-          <Icon name="user" />내 정보
+          <Icon name='user' />내 정보
         </Menu.Item>
+
         <Menu.Item
-          name="resign"
-          active={activeItem === "resign"}
+          name='resign'
+          active={activeItem === 'resign'}
           onClick={goLink}
         >
-          <Icon name="user cancel" />
+          <Icon name='user cancel' />
           회원 탈퇴
         </Menu.Item>
       </Menu>
