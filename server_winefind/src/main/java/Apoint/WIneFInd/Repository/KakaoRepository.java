be6a0a19d83fa87d192700c9d6ab521ba1939c04
UnicusStella <unicusstella@gmail.com>
2021-10-26 13:42:07 +0900

    Article : Add kakao oauth

---
+++ server_winefind/src/main/java/Apoint/WIneFInd/Repository/KakaoRepository.java
@@ -0,0 +1,56 @@
+package Apoint.WIneFInd.Repository;
+
+import Apoint.WIneFInd.Controller.KakaoAPI;
+import Apoint.WIneFInd.Model.Consumer;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
+import java.util.Date;
+import java.util.HashMap;
+
+@Repository
+@Transactional
+public class KakaoRepository {
+
+    EntityManager entityManager;
+
+    public KakaoRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    public void Create (String code, HttpSession session){
+        ModelAndView mav = new ModelAndView();
+
+        KakaoAPI kakaoApi = new KakaoAPI();
+        // 1번 인증코드 요청 전달
+        String accessToken = kakaoApi.getAccessToken(code);
+        // 2번 인증코드로 토큰 전달
+        HashMap<String, Object> userInfo = kakaoApi.getUserInfo(accessToken);
+
+        System.out.println("login info : " + userInfo.toString());
+
+        if(userInfo.get("email") != null) {
+            session.setAttribute("userId", userInfo.get("email"));
+            session.setAttribute("accessToken", accessToken);
+        }
+
+        mav.addObject("userId", userInfo.get("email"));
+        mav.setViewName("index");
+        System.out.println(mav);
+
+        Date now = new Date();
+        Consumer consumer = new Consumer();
+        consumer.setEmail(userInfo.get("email").toString());
+        consumer.setNickname(userInfo.get("nickname").toString());
+        consumer.setCreatedAt(now);
+        consumer.setUpdatedAt(now);
+        entityManager.persist(consumer);
+        entityManager.flush();
+        entityManager.close();
+    }
+}
