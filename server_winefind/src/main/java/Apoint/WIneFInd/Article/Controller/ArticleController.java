891ec66dd3d9e2f066fac9194f8c4cbf7fb2c02e
Hyungjun <ryanromaris@naver.com>
2021-11-03 14:30:57 +0900

    Dev:Merge success

--- server_winefind/src/main/java/Apoint/WIneFInd/Article/Controller/ArticleController.java
+++ server_winefind/src/main/java/Apoint/WIneFInd/Article/Controller/ArticleController.java
@@ -6,6 +6,9 @@ import Apoint.WIneFInd.Article.Service.ArticleService;
 import Apoint.WIneFInd.Wine.Model.Wine;
 import Apoint.WIneFInd.Wine.Service.WineService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
