6d2bf211ce9c1b740bbfc7165315ba14df1ef148
Hyungjun <ryanromaris@naver.com>
2021-11-14 14:08:05 +0900

    Dev: add enter signup

--- client_winefind/components/Login/Signup.js
+++ client_winefind/components/Login/Signup.js
@@ -154,6 +154,7 @@ const Signup = ({ toggleModal, changeLoginToSignup, enterkey }) => {
                   value={signUpInfo.checkPassword}
                   placeholder='Check Password'
                   onChange={handleInputValue('checkPassword')}
+                  onKeyUp={() => enterkey(createAccount)}
                 />
               </div>
             </div>
